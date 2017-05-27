1e178b662dcc4c42eb9155a4f6e25100891bed82
chuyingqifei <1425861237@qq.com>
2017-05-27 17:10:44 +0800

    secon d commit

--- main/main.js
+++ main/main.js
@@ -1,11 +1,10 @@
 module.exports = function main(str) {
   // Write your cade here
-  var len = str.length;
-  var sum = 0;
-  var i;
-  for( i = 0;i<len;i++);
-  {
-	  sum += parseInt(str.charAt(i));
-  }
-  return sum;
-};
+    var len = str.length;
+    var sum = 0;
+    for(var i = 0;i<len;i++){
+        var x = parseInt(str.charAt(i));
+        sum += x;
+    }
+    return sum;
+}
