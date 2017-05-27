dd6ca45c0c679e22cb2c797fb80b3a274bbcf294
chuyingqifei <1425861237@qq.com>
2017-05-27 16:28:03 +0800

    sum of String2

--- main/main.js
+++ main/main.js
@@ -1,6 +1,5 @@
-module.exports = function main() {
+module.exports = function main(str) {
   // Write your cade here
-  var str = arguments[0];
   var len = str.length;
   var sum = 0;
   for(var i = 0;i<len;i++);
